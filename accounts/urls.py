b0ab55f5be26d13cf21826025b28e0ba4c5b39e3
Tanvir Ahammed <tanvircolorful@gmail.com>
2025-07-30 21:24:13 +0600

    add django project and accounts app

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import home, customer, products
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('customer/', customer, name="customer"),
+    path('products/', products, name="products"),
+]
