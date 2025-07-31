a22d5cb14b6be727237896e6965a7a8e14bc9cde
Tanvir Ahammed <tanvircolorful@gmail.com>
2025-07-31 11:36:45 +0600

    set dynamic url

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from .views import home, customer, products
 
 urlpatterns = [
     path('', home, name='home'),
-    path('customer/', customer, name="customer"),
+    path('customer/<int:pk>', customer, name="customer"),
     path('products/', products, name="products"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+    
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count
+    
+    context = {'orders': orders, 'customers': customers, 'delivered': delivered,
+               'pending': pending, 'total_orders': total_orders}
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    total_orders = orders.count()
+    context = {'customer': customer, 'orders': orders, 'total_orders': total_orders}
+    return render(request, 'accounts/customer.html', context)
