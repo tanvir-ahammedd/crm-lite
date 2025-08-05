d019070959ebf2f18747e24e08dfdeb3fe1bf3e1
Tanvir Ahammed <tanvircolorful@gmail.com>
2025-08-05 10:03:55 +0600

    static files and images

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 
 def unauthenticated_user(view_func):
-    def wrapper_func(request, *args, **kwargs):
+    def wrapper_func(request, *args, **kwargs):     
         if request.user.is_authenticated:
             return redirect('home')
         else:
